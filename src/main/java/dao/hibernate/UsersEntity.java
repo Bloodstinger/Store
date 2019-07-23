47f7291b101b01a02044a2da1e8c313359cc5e59
bloodstinger <yurikko1@gmail.com>
2019-07-23 17:50:28 +0300

    Version 0.9 (added hibernate support)

---
+++ src/main/java/dao/hibernate/UsersEntity.java
@@ -0,0 +1,85 @@
+package dao.hibernate;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Table(name = "users", schema = "shop")
+public class UsersEntity {
+    private long id;
+    private String email;
+    private String password;
+    private String role;
+    private Collection<OrderEntity> ordersById;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = false, length = 255)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 255)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "role", nullable = false, length = 50)
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersEntity that = (UsersEntity) o;
+        return id == that.id &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(role, that.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, role);
+    }
+
+    @OneToMany(mappedBy = "usersByUserId")
+    public Collection<OrderEntity> getOrdersById() {
+        return ordersById;
+    }
+
+    public void setOrdersById(Collection<OrderEntity> ordersById) {
+        this.ordersById = ordersById;
+    }
+}
--- src/main/java/dao/impl/OrderDaoImpl.java
+++ src/main/java/dao/impl/OrderDaoImpl.java
@@ -3,14 +3,28 @@ package dao.impl;
 import dao.OrderDao;
 import model.Item;
 import model.Order;
+import model.User;
+import org.apache.log4j.Logger;
+import utils.DbConnector;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class OrderDaoImpl implements OrderDao {
 
+    private static final Logger logger = Logger.getLogger(OrderDaoImpl.class);
+
     @Override
     public void addOrder(Order order) {
-
+        User user = order.getUser();
+        try (Connection connection = DbConnector.connect()) {
+            Statement statement = connection.createStatement();
+            String sql = "INSERT "; //FIXME pls
+        } catch (SQLException e) {
+            logger.error("Error in adding order to db", e);
+        }
     }
 
     @Override
